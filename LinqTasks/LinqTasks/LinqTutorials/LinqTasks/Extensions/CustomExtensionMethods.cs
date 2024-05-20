9cf5fbc2b03ac5db1c962120f39f11d20b356381
s26719 <s26719@pjwstk.edu.pl>
2024-05-20 16:02:01 +0200

    dodalem zadanie

---
+++ LinqTasks/LinqTasks/LinqTutorials/LinqTasks/Extensions/CustomExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using LinqTasks.Models;
+
+namespace LinqTasks.Extensions;
+
+public static class CustomExtensionMethods
+{
+    //Put your extension methods here
+    public static IEnumerable<Emp> GetEmpsWithSubordinates(this IEnumerable<Emp> emps)
+    {
+        return emps.Where(mgr => emps.Any(sub => sub.Mgr == mgr)).OrderBy(x => x.Ename).ThenByDescending(x => x.Salary);
+    }
+}
